5351661ebf16cb40336fa476988119d41ff5e487
johan <johan.chevet@laplateforme.io>
2025-08-25 16:39:44 +0200

    nothingburger

--- database/Connection.php
+++ database/Connection.php
@@ -1,14 +1,17 @@
 <?php
 
-class Connection {
+class Connection
+{
     private static ?Connection $instance = null;
     public ?PDO $pdo = null;
 
-    private function __construct(array $config) {
+    private function __construct(array $config)
+    {
         $this->pdo = new PDO(...$config);
     }
 
-    public static function getInstance(): self {
+    public static function getInstance(): self
+    {
         if (self::$instance === null) {
             $config = include __DIR__ . "/config.php";
             self::$instance = new self($config);
